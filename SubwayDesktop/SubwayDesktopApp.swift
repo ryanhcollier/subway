c917143220c6c0c2ea191799def6ed820edd73ff
Ryan Collier <ryanhcollier@gmail.com>
2026-01-11 14:24:47 -0500

    Initial Commit

---
+++ SubwayDesktop/SubwayDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubwayDesktopApp.swift
+//  SubwayDesktop
+//
+//  Created by Ryan Collier on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SubwayDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
